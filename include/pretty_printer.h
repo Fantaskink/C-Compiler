13f7d8cb638034f2607f3b6ce3927bc6a182470e
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2024-06-25 15:53:00 +0200

    Update pretty_printer.h

--- include/pretty_printer.h
+++ include/pretty_printer.h
@@ -4,12 +4,19 @@
 #include "parser.h"
 
 static const char *ASTNodeTypeStrings[AST_NUM_TYPES] = {
-    "AST_TRANSLATION_UNIT", "AST_FUNCTION_DEF", "AST_FUNCTION_DECL",
-    "AST_TYPE_SPEC",        "AST_IDENTIFIER",   "AST_PARAM_LIST",
-    "AST_PARAM_DECL",       "AST_DECL",         "AST_INT_LITERAL",
-    "AST_FLOAT_LITERAL",    "AST_UNKNOWN",
+    "Translation Unit",
+    "Function Definition",
+    "Function Declaration",
+    "Type Specifier",
+    "Identifier",
+    "Parameter List",
+    "Parameter Declaration",
+    "Declaration",
+    "Int Literal",
+    "Float Literal",
+    "UNKNOWN",
 };
 
 void print_ast(ASTNode_t *ast);
 
-#endif // !
+#endif //! PRETTY_PRINTER
